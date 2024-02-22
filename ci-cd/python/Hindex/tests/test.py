1d602918c8b224fd1c5694ba0d88515c00b10bd6
ThatsRightImKiryu <kirill.korneev.81@mail.ru>
2024-02-22 18:14:03 +0300

    added some bash and correct tests

---
+++ ci-cd/python/Hindex/tests/test.py
@@ -0,0 +1,36 @@
+import unittest
+from src.solution import Solution
+
+class SquareEqSolverTestCase(unittest.TestCase):
+    
+    def test_zero(self):
+       res = Solution().hIndex([0])
+       self.assertEqual(res, 0)
+    
+    def test_one(self):
+       res = Solution().hIndex([1])
+       self.assertEqual(res, 1)
+    
+    def test_one_two(self):
+       res = Solution().hIndex([1, 2])
+       self.assertEqual(res, 1)
+    
+    def test_two_three(self):
+       res = Solution().hIndex([2, 3])
+       self.assertEqual(res, 2)
+    
+    def test_three_two(self):
+       res = Solution().hIndex([3, 2])
+       self.assertEqual(res, 2) 
+
+    def test_three_three(self):
+       res = Solution().hIndex([3, 3])
+       self.assertEqual(res, 2) 
+    
+    def test_long_arr(self):
+       res = Solution().hIndex([4, 1, 2, 5, 4])
+       self.assertEqual(res, 3) 
+
+    def test_empty(self):
+       res = Solution().hIndex([])
+       self.assertEqual(res, 0) 
